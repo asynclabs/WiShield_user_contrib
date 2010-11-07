ae22cc182ab10fda1e19536974298a133e19af73
GregEigsti <greg@eigsti.com>
2010-11-07 09:56:21 -0800

    jrwifi changes to make WiServer more stable. Removes malloc() and replaces with global char buffer to hold URL/GET data to be passed to WiServer/SimpleServer app's page generation callback function.

--- apps-conf.h
+++ apps-conf.h
@@ -41,7 +41,8 @@
 //
 // Application type defines; uncomment to enable APP_TYPES
 //   APP_UDPAPP is used for UDP only apps as well as DNS and DHCP apps; if your app will use
-//   DNS and/or DHCP then your APP_TYPE (e.g. APP_WISERVER) AND APP_UDPAPP must be defined (uncommented).
+//   DNS and/or DHCP then your APP_TYPE (e.g. APP_WISERVER) AND APP_UDPAPP must be defined 
+//   (uncommented). Currently only APP_UDPAPP may be defined at the same time as any other APP_TYPE
 //
 //#define APP_WEBSERVER
 //#define APP_WEBCLIENT
@@ -52,20 +53,26 @@
 //
 // Add on features; uncomment to enable additional functionality
 //
-//#define UIP_DNS                //Add DNS capabilities - APP_UDPAPP must be defined!
-//#define UIP_DHCP               //Add DHCP capabilities - APP_UDPAPP must be defined!
-//#define UIP_SCAN               //Add Access Point scan capabilities
+//#define UIP_DNS                  // Add DNS capabilities - APP_UDPAPP must be defined!
+//#define UIP_DHCP                 // Add DHCP capabilities - APP_UDPAPP must be defined!
+//#define UIP_SCAN                 // Add Access Point scan capabilities
+
+//
+// Commonly accessed WiServer settings 
+//
+#define WISERVER_GET_STRING_MAX 64 // Length of buffer which holds URL/GET passed to WiServer
+                                   // SimpleServer sendMyPage() page serving function
 
 //
 // Commonly accessed uIP stack settings 
 //
-#define UIP_WEP_KEY_LEN     13 // WEP Key length: 5 bytes (64-bit WEP); 13 bytes (128-bit WEP)
-#define UIP_WEP_KEY_DEFAULT 0  // Default WEP key ID: Key 0, 1, 2, 3
-#define MAX_TCP_CONNS       2  // Max TCP connections desired
-#define MAX_TCP_LISTENPORTS 2  // Max TCP listening ports
-#define MAX_UDP_CONNS       1  // Max UDP connections desired
+#define UIP_WEP_KEY_LEN         13 // WEP Key length: 5 bytes (64-bit WEP); 13 bytes (128-bit WEP)
+#define UIP_WEP_KEY_DEFAULT      0 // Default WEP key ID: Key 0, 1, 2, 3
+#define MAX_TCP_CONNS            2 // Max TCP connections desired
+#define MAX_TCP_LISTENPORTS      2 // Max TCP listening ports
+#define MAX_UDP_CONNS            1 // Max UDP connections desired
 // Don't play with UIP_CLOCK_DIV unless you know what you are doing!
-#define UIP_CLOCK_DIV       2  // Referenced in stack.c; default 2
+#define UIP_CLOCK_DIV            2 // Referenced in stack.c; default 2
 
 // -- End uIP/WiShield stack configuration settings
 // ----------------------------------------------------------------------------
