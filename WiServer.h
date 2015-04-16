da886e0defbba888d687f8f63073ea6ae0f485a0
Andre Courchesne <andre.courchesne@intello.com>
2015-04-16 13:23:57 -0400

    - Modifications required to compile under Arduino 1.6.3 (at least the WebServer example)

--- WiServer.h
+++ WiServer.h
@@ -57,7 +57,7 @@ typedef void (*returnFunction)(char* data, int len);
 /*
  * Function for serving web pages
  */
-typedef boolean (*pageServingFunction)(char* URL);
+typedef bool (*pageServingFunction)(char* URL);
 
 /*
  * Function for providing the body of a POST request
@@ -103,7 +103,7 @@ class GETrequest
 		 * or is currently connected and communicating with the server).  If it is, any calls that attempt
 		 * to change the properties of the request will be ignored.
 		 */
-		boolean isActive();
+		bool isActive();
 
 		/*
 		 * Sets the URL for the request.  Calls to this method will be ignored if the request
@@ -124,7 +124,7 @@ class GETrequest
 	    // Return value callback function (may be NULL)
 		returnFunction returnFunc;
 	    // Indicates if the request is currently active (i.e. has a connection)
-		boolean active;
+		bool active;
 	    // Body data callback function (may be NULL)
 	    bodyFunction body;
 	    // Body preamble (may be NULL)
@@ -185,7 +185,7 @@ class Server: public Print
 		 * will output log info via the Serial class.  Verbose mode is disabled by
 		 * default, but is automatically enabled if DEBUG is defined
 		 */
-		void enableVerboseMode(boolean enable);
+		void enableVerboseMode(bool enable);
 
 		/**
 		 * The server task method (must be called in the main loop to run the WiServer)
@@ -195,7 +195,7 @@ class Server: public Print
 		/**
 		 * Writes a single byte to the current connection buffer
 		 */
-		virtual void write(uint8_t);
+		virtual size_t write(uint8_t);
 
 		/**
 		 * Prints a string that is stored in program memory
@@ -224,7 +224,7 @@ class Server: public Print
 		 * in the network.  Changes to the content of the page should only be made if this method
 		 * returns false.
 		 */
-		boolean sendInProgress();
+		bool sendInProgress();
 
 		/**
 		 * Checks if the client for the current server request resides on the same local network
@@ -239,7 +239,7 @@ class Server: public Print
 		 * Note that security checks based on the client IP address are not 100% reliable,
 		 * so this feature should not be relied upon to control access to sensitive data!
 		 */
-		boolean clientIsLocal();
+		bool clientIsLocal();
 
 		/**
 		 * Sets the pins used to indicate TX and RX activity over the network.
--- examples/WebServer/WebServer.pde
+++ examples/WebServer/WebServer.pde
@@ -7,17 +7,17 @@
 #include <WiShield.h>
 
 // Wireless configuration parameters ----------------------------------------
-unsigned char local_ip[]    = {192,168,1,2};   // IP address of WiShield
-unsigned char gateway_ip[]  = {192,168,1,1};   // router or gateway IP address
+unsigned char local_ip[]    = {10,0,9,44};   // IP address of WiShield
+unsigned char gateway_ip[]  = {10,0,9,1};   // router or gateway IP address
 unsigned char subnet_mask[] = {255,255,255,0}; // subnet mask for the local network
-char ssid[]                 = {"ASYNCLABS"};   // max 32 bytes
+char ssid[]                 = {"NintendoDS"};   // max 32 bytes
 unsigned char security_type = 0;               // 0 - open; 1 - WEP; 2 - WPA; 3 - WPA2
 
 // WPA/WPA2 passphrase
-const prog_char security_passphrase[] PROGMEM = {"12345678"};	// max 64 characters
+const char PROGMEM security_passphrase[] PROGMEM = {"Etc1brew!"};	// max 64 characters
 
 // WEP 128-bit keys
-prog_uchar wep_keys[] PROGMEM = { 
+const unsigned char PROGMEM wep_keys[] PROGMEM = { 
 	0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,	// Key 0
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Key 1
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	// Key 2
@@ -39,7 +39,7 @@ void setup()
 }
 
 // This is the webpage that is served up by the webserver
-const prog_char webpage[] PROGMEM = {"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<center><h1>Hello World!! I am WiShield</h1><form method=\"get\" action=\"0\">Toggle LED:<input type=\"submit\" name=\"0\" value=\"LED1\"></input></form></center>"};
+const char PROGMEM webpage[] PROGMEM = {"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<center><h1>Hello World!! I am WiShield</h1><form method=\"get\" action=\"0\">Toggle LED:<input type=\"submit\" name=\"0\" value=\"LED1\"></input></form></center>"};
 
 void loop()
 {
