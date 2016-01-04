b30a205f4757a2f51a3721d69ddce4382e20240d
hamityanik <hamityanilk@hotmail.com>
2016-01-04 22:57:39 +0200

    DNS capabilities activated

--- apps-conf.h
+++ apps-conf.h
@@ -47,13 +47,13 @@
 //#define APP_WEBSERVER
 //#define APP_WEBCLIENT
 //#define APP_SOCKAPP
-//#define APP_UDPAPP
+#define APP_UDPAPP
 #define APP_WISERVER
 
 //
 // Add on features; uncomment to enable additional functionality
 //
-//#define UIP_DNS                  // Add DNS capabilities - APP_UDPAPP must be defined!
+#define UIP_DNS                  // Add DNS capabilities - APP_UDPAPP must be defined!
 //#define UIP_DHCP                 // Add DHCP capabilities - APP_UDPAPP must be defined!
 //#define UIP_SCAN                 // Add Access Point scan capabilities
 
